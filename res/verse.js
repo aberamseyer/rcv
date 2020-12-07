294adb99cfc4dce4a41fe6f04e7f34892aafffd8
Abe Ramseyer <abe.ramseyer@gmail.com>
2020-12-07 15:48:38 -0600

    permalink for verse page

--- res/verse.js
+++ res/verse.js
@@ -1,35 +1,45 @@
-const verseContainer = document.getElementById('verses');
-const recognizedVerses = document.getElementById('recognized-verses');
-const exceededMax = document.getElementById('exceeded-max');
-const verseInput = document.getElementById('verse-input');
+(() => {
+	const verseContainer = document.getElementById('verses');
+	const recognizedVerses = document.getElementById('recognized-verses');
+	const exceededMax = document.getElementById('exceeded-max');
+	const verseInput = document.getElementById('verse-input');
 
-let requestTimer = 0;
-verseInput.onkeyup = e => {
+	let requestTimer = 0;
+	verseInput.onkeyup = e => {
 
-	const { key } = e;
+		const { key } = e;
 
-	if (verseInput.value.length > 3 && /^\w$/.test(key) || 1) { // TODO make better
-		const formData = new FormData();
-		formData.append('action', 'request');
-		formData.append('q', verseInput.value.trim());
+		if (verseInput.value.length > 3 && /^\w$/.test(key) || key === 'Backspace') {
+			const formData = new FormData();
+			formData.append('action', 'request');
+			formData.append('q', verseInput.value.trim());
 
-		const request = new XMLHttpRequest();
-		request.open("POST", "/ajax");
+			const request = new XMLHttpRequest();
+			request.open("POST", "/ajax");
 
-		request.onloadend = () => {
-			if (request.status === 200) {
-				const { results, q } = JSON.parse(request.response);
-				verseContainer.innerHTML = results
-					.map(res => 
-						`<div><a target='_blank' href='${res.href}'>
-							<small><b>${res.reference}</b>: ${res.text}</small></a>
-						</div>`)
-					.join('');
-				recognizedVerses.innerText = results.map(res => res.reference).join(', ');
+			request.onloadend = () => {
+				if (request.status === 200) {
+					const { results, q } = JSON.parse(request.response);
+					verseContainer.innerHTML = results
+						.map(res => 
+							`<div><a target='_blank' href='${res.href}'>
+								<small><b>${res.reference}</b>: ${res.text}</small></a>
+							</div>`)
+						.join('');
+					recognizedVerses.innerText = results.map(res => res.reference).join('; ');
+
+					if (results.length) {
+						recognizedVerses.innerHTML += `<small>
+							<a href='' onclick='navigator.clipboard.writeText("https://rcv.ramseyer.dev/verse?verses=${encodeURIComponent(recognizedVerses.innerText)}"); return false;'>Copy link to these verses to clipboard</a></small>`;
+					}
+				}
 			}
-		}
 
-		clearTimeout(requestTimer);
-		requestTimer = setTimeout(() => request.send(formData), 200);
+			clearTimeout(requestTimer);
+			requestTimer = setTimeout(() => request.send(formData), 200);
+		}
 	}
-}
+
+	if (verseInput.value.length)
+			verseInput.onkeyup({ key: 'a' }); // manually trigger on load
+})();
--- verse.php
+++ verse.php
@@ -6,16 +6,16 @@
  * Time: 12:44
  */
 
-// TODO fix parsing, permalink to list of verses
-
 $title = "Verse Requester";
 require "init.php";
 require "head.php";
+
+$permalink = $_GET['verses'];
 ?>
 
 <h2><a href='/bible'>Verse Requester</a></h2>
 
-<input id='verse-input' name='q' type='text' style='width: 100%;' placeholder='e.g., Gen. 1:26; John 1:1, 14; 2 Cor. 3:18; Jude 20-21'>
+<input id='verse-input' name='q' type='text' style='width: 100%;' placeholder='e.g., Gen. 1:26; John 1:1, 14; 2 Cor. 3:18; Jude 20-21' value='<?= $permalink ?: ''?>'>
 <small>Note: you can request a maximum of 200 verses at a time</small>
 <span>Recognized verses: <small id='recognized-verses' style="display: inline"></small></span>
 
