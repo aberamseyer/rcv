798471527ed48aa4ddc88acfa682981e9dc8385f
Abe Ramseyer <abe.ramseyer@gmail.com>
2021-01-28 23:16:25 -0600

    caching in memory for 1 hour

--- inc/cache-head.php
+++ inc/cache-head.php
@@ -15,8 +15,19 @@ if (!isset($_GET['no_cache']) && !LOCAL) {
 	$filename = str_replace('/', '-', $url)."_".($serif_text ? 1 : 0)."-".($light_theme ? 1 : 0)."-".($minimal_layout ? 1 : 0).".html";
 	$cachekey = $_SERVER['DOCUMENT_ROOT']."/extras/cache/".$filename;
 
-	if (file_exists($cachekey)) {
-		readfile($cachekey);
+	if (
+		($output = $redis_client->get("rcv.ramseyer.dev/cache".$cachekey)) ||
+		file_exists($cachekey)
+	) {
+		if ($output) {
+			// $output contains page from memory
+		}
+		else { // file exists, cache it in memory
+			$output = file_get_contents($cachekey);
+			$redis_client->set("rcv.ramseyer.dev/cache".$cachekey, $output);
+			$redis_client->expire("rcv.ramseyer.dev/cache".$cachekey, 60 * 60); // cache pages for 1 hour in memory
+		}
+		echo $output;
 		echo "\n<!-- Cached copy $filename, generated ".date('Y-m-d H:g:i', filemtime($cachekey))." -->";
 		exit;
 	}
