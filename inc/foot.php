81066aa8af8dc23790778392614a5954e824c95f
Abe Ramseyer <abe.ramseyer@gmail.com>
2021-02-22 12:14:33 -0600

    sans verse references on search page and menu show/hide on scroll

--- inc/foot.php
+++ inc/foot.php
@@ -6,6 +6,22 @@
             </div>
         </div>
         <script src='/res/js/search.js'></script>
+        <script>
+            // menu show/hide on scroll
+            (() => {
+                const menu = document.getElementById('menu');
+                let scrollPos = 0;
+                window.addEventListener('scroll', () => {
+                    const rect = document.body.getBoundingClientRect();
+                    if (rect.top > scrollPos)
+                        menu.classList.remove('hide');
+                    else
+                        menu.classList.add('hide');
+
+                    scrollPos = rect.top;
+                });
+            })();
+        </script>
         <!--
 	    This is solely a personal project.
             If you have questions, concerns, or find a mistake, please email me: abe(at)ramseyer(dot)dev.
